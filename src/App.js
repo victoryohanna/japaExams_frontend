7bc197f98825592bdc19c199fae3d46313bec621
Victor Yohanna <victoryohanna@gmail.com>
2023-04-14 22:21:12 +0100

    creates faqs routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import AboutUs from "./pages/About";
 import GRE from "./pages/exams/Gre";
-import NotFound from './pages/NotFound';
-
+import NotFound from "./pages/NotFound";
+import FAQs from "./pages/FAQs";
+import Qgre from "./components/elements/faqs/Qgre";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/about' element={<AboutUs/>}/>
-        <Route exact path='/gre' element={<GRE/>}/>
-        <Route path='*' element={<NotFound/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<AboutUs />} />
+        <Route exact path="/gre" element={<GRE />} />
+        <Route exact path="/faqs" element={<FAQs />}>
+          <Route path="/faqs/gre" element={<Qgre />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
