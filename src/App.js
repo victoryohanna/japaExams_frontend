5bdb6ab09fefa236c0d2e3e800ffceceb1ffb8ce
Victor Yohanna <victoryohanna@gmail.com>
2023-04-14 06:03:03 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
+import AboutUs from "./pages/About";
+import GRE from "./pages/exams/Gre";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/about' element={<AboutUs/>}/>
+        <Route exact path='/gre' element={<GRE/>}/>
+      </Routes>
+      <Footer/>
+      
     </div>
   );
 }
