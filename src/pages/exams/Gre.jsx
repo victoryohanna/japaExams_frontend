5b3d9ab8852fb82679db5ac210d5f772b1678a33
Victor Yohanna <victoryohanna@gmail.com>
2023-04-14 13:28:27 +0100

    modified GRE component

--- src/pages/exams/Gre.jsx
+++ src/pages/exams/Gre.jsx
@@ -7,7 +7,7 @@ const GRE = () => {
         <h2>GRE Exam 2023 </h2>
         <h5>
           [Dates, Registration, Fees, Eligibility, Syllabus, Pattern,
-          Preparation, Result & Score]
+          Preparation, Result & Score] 
         </h5>
       </div>
       <div className="gre-details ">
@@ -28,8 +28,8 @@ const GRE = () => {
           analytical writing skills. The computer-based exam takes about 3 hours
           and 45 minutes to complete and is divided into three main sections:
           Verbal Reasoning, Quantitative Reasoning, and Analytical Writing.
-        </p>
-        <p>
+        </p> 
+        <p> 
           The Verbal Reasoning section evaluates your ability to analyze and
           evaluate written material and synthesize information from it. The
           Quantitative Reasoning section assesses your problem-solving ability
@@ -262,6 +262,231 @@ const GRE = () => {
                   </tr>
                 </tbody>
               </table>
+            </p>
+          </div>
+        </div>
+        <div className="collapse-content">
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#myth-burster"
+          >
+            <h6>2023 GRE Exam: Myth-Bursters - Myths Vs Realities of GRE</h6>
+          </button>
+          <div className="collapse" id="myth-burster">
+            <p>
+              In addition to the tips and resources mentioned above, it's
+              important to also address some common myths and misconceptions
+              about the GRE exam. These myths can often create unnecessary
+              anxiety and confusion for potential test-takers, including you, so
+              it's important to clarify them:
+            </p>
+            <ul>
+              <li>
+                The GRE is all about memorization.
+                <p>
+                  Reality While the GRE does require a certain level of
+                  knowledge, it is not a test of memorization. Rather, it is
+                  designed to assess your ability to reason, analyze, and draw
+                  conclusions based on the information provided.
+                </p>
+              </li>
+              <li>
+                You need to have a background in math to do well on the GRE.
+                <p>
+                  Reality: While the GRE does include math questions, it is not
+                  necessary to have a background in math to do well on the test.
+                  The math questions are designed to test your ability to reason
+                  and apply mathematical concepts, rather than your ability to
+                  perform complex calculations.
+                </p>
+              </li>
+              <li>
+                The verbal section is all about vocabulary.
+                <p>
+                  Reality: While having a strong vocabulary can certainly be
+                  helpful on the verbal section of the GRE, it is not the only
+                  factor that determines success. The verbal section also
+                  includes questions on reading comprehension, critical
+                  reasoning, and analytical writing.
+                </p>
+              </li>
+              <li>
+                The GRE is only important if you're applying to graduate school.
+                <p>
+                  Reality: While the GRE is often required for admission to
+                  graduate programs, it can also be used for other purposes,
+                  such as qualifying for scholarships, fellowships, or research
+                  positions.
+                </p>
+              </li>
+              <li>
+                You only get one chance to take the GRE.
+                <p>
+                  Reality: You can take the GRE multiple times, and many
+                  graduate programs will only consider your highest score.
+                  However, it's important to keep in mind that taking the GRE
+                  too many times can reflect poorly on your application.
+                </p>
+              </li>
+              <li>
+                The computer-delivered GRE is easier than the paper-based
+                version.
+                <p>
+                  Reality: While the format of the test may be different, the
+                  content and difficulty level of the computer-delivered GRE is
+                  the same as the paper-based version. It's important to choose
+                  the format that you are most comfortable with and to prepare
+                  accordingly.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="collapse-content">
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#myth-burster2"
+          >
+            <h6>
+              GRE vs GMAT IN 2023: Should you register for the GRE or register
+              for the GMAT?
+            </h6>
+          </button>
+          <div className="collapse" id="myth-burster2">
+            <p>
+              Both the GRE and the GMAT are similar, but different exams. They
+              are similar because they both focus on the aptitude of test takers
+              to measure their readiness for Masters or PhD studies abroad.
+              However, they differ in the area of applicability. The GMAT is
+              appropriate or right for you if you are interested in graduate
+              business schools and management studies like the MBA. On the other
+              hand, you should register for the GRE and take the GRE if your
+              program is either a STEM (Science, Technology, Engineering and
+              Mathematics) program or not a business-related program. Some
+              disciplines like Economics and Law also require the GRE. Also note
+              that some schools accept the GMAT as an alternative to the GRE
+              while others are somewhat rigid about their preferences. It is
+              best to look up the requirements on the website of the program or
+              university you are applying to.
+            </p>
+          </div>
+        </div>
+        <div className="collapse-content">
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#myth-burster3"
+          >
+            <h6>Preparing for the GRE Exam in 2023-Tips and Strategies</h6>
+          </button>
+          <div className="collapse" id="myth-burster3">
+            <p>
+              On test day, it's important to arrive early, bring all necessary
+              documents, and follow all test center rules. It's also important
+              to pace yourself during the exam and manage your time wisely.
+              Finally, be sure to review your answers and make any necessary
+              changes before submitting your test. Good enough, it’s not yet
+              test date for you! Now let's dive deeper into each section of the
+              GRE exam and explore some tips and strategies to help you prepare
+              for success on your exam day. These are the tips I wish I knew
+              before I took the test the first time:
+            </p>
+            <span>1. Verbal Section:</span>
+            <ul>
+              <li>
+                Expand your vocabulary: Learning new words and their meanings is
+                essential for doing well on the verbal section. Use flashcards,
+                read widely, and practice using new words in context.
+              </li>
+              <li>
+                Practice reading comprehension: The verbal section includes
+                several reading comprehension questions. Practice reading
+                articles, essays, and other types of academic writing and taking
+                notes to help you retain the main points and key details.
+              </li>
+              <li>
+                Use process of elimination: If you're unsure of an answer,
+                eliminate the options that you know are incorrect. This can
+                increase your chances of guessing the right answer and can help
+                you avoid getting stuck on difficult questions.
+              </li>
+            </ul>
+            <span>2. Quantitative Section:</span>
+            <ul>
+              <li>
+                Brush up on math basics: Make sure you're comfortable with
+                algebra, geometry, and basic arithmetic before tackling more
+                complex math problems. Use online resources or textbooks to
+                review these topics.
+              </li>
+              <li>
+                Memorize important formulas: Knowing key formulas, such as the
+                quadratic equation or the Pythagorean theorem, can save you time
+                on the test and help you avoid careless mistakes.
+              </li>
+              <li>
+                Solve problems step by step: Don't rush through math problems.
+                Take the time to read the question carefully and solve it step
+                by step. This can help you avoid mistakes and make it easier to
+                identify errors if you get the wrong answer.
+              </li>
+            </ul>
+            <span>3. Analytical Writing Section:</span>
+            <ul>
+              <li>
+                Practice outlining: Before you start writing, create an outline
+                for your essay. This can help you organize your thoughts and
+                ensure that you're addressing all of the key points.
+              </li>
+              <li>
+                Use specific examples: When making an argument or illustrating a
+                point, use specific examples to support your ideas. This can
+                make your essay more compelling and convincing.
+              </li>
+              <li>
+               
+                Focus on clarity: Your writing should be clear, concise, and
+                well-organized. Use transitions between paragraphs, avoid overly
+                complex sentences, and proofread carefully to ensure that your
+                ideas are conveyed effectively.
+              </li>
+            </ul>
+            <span>Extras tips for test day…</span>
+            <ul>
+              <li>
+                Manage your time wisely: On test day, it's important to pace
+                yourself and manage your time wisely. You don't want to spend
+                too much time on one question or section and run out of time
+                before completing the entire exam.
+              </li>
+              <li>
+              
+                Stay calm and focused: Test days can be stressful, but it's
+                important to stay calm and focused. Take deep breaths, stay
+                hydrated, and stay focused on the task at hand.
+              </li>
+              <li>
+            
+                Get plenty of rest: Finally, it's important to get plenty of
+                rest in the days leading up to the exam. A good night's sleep
+                can help you feel refreshed and energized on test day.
+              </li>
+            </ul>
+            <p>
+              We understand that preparing for the GRE exam can be a daunting
+              task, but with the right tools and strategies, you can feel
+              confident and prepared to take on this important challenge.
+              Whether you're studying on your own or working with a tutor, we
+              encourage you to stay motivated, stay focused, and keep your goals
+              in mind as you prepare for this important exam. The tips and
+              strategies highlighted so far will go a long way in making you
+              feel confident and prepared to take on the GRE exam. On this GRE
+              website, we offer a variety of resources and tools to help you
+              prepare for each section of the exam. Whether you're looking to
+              register for the GRE Exam, need practice tests, study materials,
+              or tips from other test-takers and experts, we've got you covered.
             </p>
           </div>
         </div>
