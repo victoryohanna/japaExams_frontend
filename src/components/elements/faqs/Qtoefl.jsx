adcb54474a9d29ed390f7d024f137f159bbc53da
Victor Yohanna <victoryohanna@gmail.com>
2023-04-30 10:50:16 +0100

    add toefl Q&A

--- src/components/elements/faqs/Qtoefl.jsx
+++ src/components/elements/faqs/Qtoefl.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import "../../../sass/faqs/qtoefl.scss";
 
 const Qtoefl = () => {
   return (
-    <div className="qgre-container">
+    <div className="qtoefl-container">
       <p>
         You might be interested in exploring some of these Frequently Asked
         Questions (FAQs) that other applicants have asked us in the past at Japa
@@ -14,11 +15,11 @@ const Qtoefl = () => {
           className="dropdown-toggle"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#faqs_gre1"
+          data-bs-target="#faqs_toefl1"
         >
           Q: What is the TOEFL exam?
         </Link>
-        <div className="collapse" id="faqs_gre1">
+        <div className="collapse" id="faqs_toefl1">
           <p>
             <span>Answer:</span> The TOEFL exam is a standardized test that
             measures the English language proficiency of non-native speakers. It
@@ -33,11 +34,11 @@ const Qtoefl = () => {
           className="dropdown-toggle"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#faqs_gre2"
+          data-bs-target="#faqs_toefl2"
         >
           Q: When can I take the TOEFL Exam?
         </Link>
-        <div className="collapse" id="faqs_gre2">
+        <div className="collapse" id="faqs_toefl2">
           <p>
             <span>Answer:</span>{" "}
             <p>
@@ -69,11 +70,11 @@ const Qtoefl = () => {
           className="dropdown-toggle"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#faqs_gre3"
+          data-bs-target="#faqs_toefl3"
         >
           Q: What do I need to take the TOEFL centre?
         </Link>
-        <div className="collapse" id="faqs_gre3">
+        <div className="collapse" id="faqs_toefl3">
           <p>
             <span>Answer:</span>
             <p>
@@ -117,11 +118,11 @@ const Qtoefl = () => {
           className="dropdown-toggle"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#faqs_gre4"
+          data-bs-target="#faqs_toefl4"
         >
           Q: How are TOEFL scores calculated?
         </Link>
-        <div className="collapse" id="faqs_gre4">
+        <div className="collapse" id="faqs_toefl4">
           <p>
             <span>Answer:</span>
             <p>
@@ -156,11 +157,11 @@ const Qtoefl = () => {
           className="dropdown-toggle"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#faqs_gre5"
+          data-bs-target="#faqs_toefl5"
         >
           Q: How do you advise I prepare for the forthcoming TOEFL Exam?
         </Link>
-        <div className="collapse" id="faqs_gre5">
+        <div className="collapse" id="faqs_toefl5">
           <p>
             <span>Answer:</span>
           </p>
@@ -183,11 +184,419 @@ const Qtoefl = () => {
               articles, watch English-language movies and TV shows, and practice
               speaking with native English speakers whenever possible.
             </li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>
+              Take practice tests: Practice tests are a great way to get a sense
+              of what the actual exam will be like. They can also help you
+              identify areas where you need more practice and build your
+              confidence.
+            </li>
+            <li>
+              Set goals: Before you start studying, set specific goals for what
+              you want to achieve. This can help you stay motivated and focused
+              as you prepare for the exam.
+            </li>
+            <li>
+              Find a study partner: Studying with a partner or group can be a
+              great way to stay motivated and get feedback on your progress.
+              Consider finding a language exchange partner or joining a study
+              group in your area.
+            </li>
+            <li>
+              Stay organized: As you study, make sure to keep all of your notes,
+              practice tests, and study materials organized. This can help you
+              stay focused and avoid feeling overwhelmed.
+            </li>
+            <li>
+              Manage your time wisely: On test day, it's important to pace
+              yourself and manage your time wisely. You don't want to spend too
+              much time on one question or section and run out of time before
+              completing the entire exam.
+            </li>
+            <li>
+              Stay calm and focused: Test days can be stressful, but it's
+              important to stay calm and focused. Take deep breaths, stay
+              hydrated, and stay focused on the task at hand.
+            </li>
+            <li>
+              Get plenty of rest: Finally, it's important to get plenty of rest
+              in the days leading up to the exam. A good night's sleep can help
+              you feel refreshed and energized on test day.
+            </li>
           </ul>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl6"
+        >
+          Q: What is a good TOEFL score?
+        </Link>
+        <div className="collapse" id="faqs_toefl6">
+          <p>
+            <span>Answer:</span>
+            <p>
+              In general, a score of 100 or higher is considered a good score on
+              the TOEFL test. However, the minimum score required for admission
+              to a particular university or program may vary, so it is important
+              for you to check the requirements of the institutions you are
+              interested in attending. Some schools accept 70-80 as good scores
+              too. The higher your score, the better.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl7"
+        >
+          Q: How long does the TOEFL exam take?
+        </Link>
+        <div className="collapse" id="faqs_toefl7">
+          <p>
+            <span>Answer:</span>
+            <p>The TOEFL exam is approximately four hours long.</p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl8"
+        >
+          Q: How is the TOEFL exam administered?
+        </Link>
+        <div className="collapse" id="faqs_toefl8">
+          <p>
+            <span>Answer:</span>
+            <p>
+              The TOEFL exam is administered via a computer-based test or a
+              paper-based test, depending on the location.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl9"
+        >
+          Q: How is the TOEFL exam scored?
+        </Link>
+        <div className="collapse" id="faqs_toefl9">
+          <p>
+            <span>Answer:</span>
+            <p>
+              The TOEFL exam is scored on a scale of 0-120. Each section of the
+              exam (reading, listening, speaking, and writing) is scored
+              separately each on a scale of 0 to 30, and the final score is an
+              average of these four section scores.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl10"
+        >
+          Q: How much does the TOEFL exam cost?
+        </Link>
+        <div className="collapse" id="faqs_toefl10">
+          <p>
+            <span>Answer:</span>
+            <p>
+              The exam body, ETS, charges standard fees in dollars for the TOEFL
+              test. Therefore, the cost of the TOEFL exam varies with the
+              exchange rate. Find the prices of all our exams, including the
+              TOEFL, <a href="/">here</a>. There are also additional fees for
+              services such as rescheduling, changing test centers, or score
+              reporting.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl11"
+        >
+          Q: How can I register for the TOEFL exam?
+        </Link>
+        <div className="collapse" id="faqs_toefl11">
+          <p>
+            <span>Answer:</span>
+            <p>You can register for the TOEFL exam with us at Japa Exams.</p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl12"
+        >
+          Q: How often can I take the TOEFL exam?
+        </Link>
+        <div className="collapse" id="faqs_toefl12">
+          <p>
+            <span>Answer:</span>
+            <p>
+              The TOEFL exam is offered multiple times a year, typically once a
+              week.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl13"
+        >
+          Q: How long is my TOEFL exam score valid?
+        </Link>
+        <div className="collapse" id="faqs_toefl13">
+          <p>
+            <span>Answer:</span>
+            <p>
+              The TOEFL exam score is typically valid for two years from the
+              date you take the exam.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl14"
+        >
+          Q: What means of identification do I need for the TOEFL exam on test
+          date?
+        </Link>
+        <div className="collapse" id="faqs_toefl14">
+          <p>
+            <span>Answer:</span>
+            <p>
+              A valid international passport is the identification document
+              accepted in Nigeria. Expired passports or passports that do not
+              match your registration details exactly will be rejected. Avoid
+              such errors.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl15"
+        >
+          Q: Can I retake the TOEFL exam?
+        </Link>
+        <div className="collapse" id="faqs_toefl15">
+          <p>
+            <span>Answer:</span>
+            <p>
+              Yes, you can retake the TOEFL exam as many times as you wish, but
+              there are certain restrictions on how often you can take the exam.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl16"
+        >
+          Q: What is the minimum TOEFL score I need to get admitted to
+          universities and colleges abroad?
+        </Link>
+        <div className="collapse" id="faqs_toefl16">
+          <p>
+            <span>Answer:</span>
+            <p>
+              The minimum TOEFL score required for admission to universities and
+              colleges varies by institution, but it typically ranges from
+              80-100.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl17"
+        >
+          Q: How long does it take to receive my TOEFL exam scores?
+        </Link>
+        <div className="collapse" id="faqs_toefl17">
+          <p>
+            <span>Answer:</span>
+            <p>
+              TOEFL exam scores are typically available online within 10 days
+              after the test date.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl18"
+        >
+          Q: Can I send my TOEFL exam scores to multiple universities and
+          colleges?
+        </Link>
+        <div className="collapse" id="faqs_toefl18">
+          <p>
+            <span>Answer:</span>
+            <p>
+              Yes, you can send your TOEFL exam scores to multiple universities
+              and colleges for a fee.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl19"
+        >
+          Q: Can I request a re-score of my TOEFL exam?
+        </Link>
+        <div className="collapse" id="faqs_toefl19">
+          <p>
+            <span>Answer:</span>
+            <p>
+              Yes, you can request a re-score of your TOEFL exam, but at a fee.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl20"
+        >
+          Q: Can I cancel my TOEFL exam score?
+        </Link>
+        <div className="collapse" id="faqs_toefl20">
+          <p>
+            <span>Answer:</span>
+            <p>
+              Yes, you can cancel your TOEFL exam score before leaving the
+              testing center, but there is a fee for doing so.
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl21"
+        >
+          Q: How long is the TOEFL exam?
+        </Link>
+        <div className="collapse" id="faqs_toefl21">
+          <p>
+            <span>Answer:</span>
+            <p>
+              The TOEFL exam can last anywhere from 3.5 to 4 hours, depending on
+              the number of questions in each section
+            </p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl22"
+        >
+          Q: How long are TOEFL scores valid?
+        </Link>
+        <div className="collapse" id="faqs_toefl22">
+          <p>
+            <span>Answer:</span>
+            <p>TOEFL scores are valid for two years from the test date</p>
+          </p>
+        </div>
+      </div>
+
+      <div className="collapse-content">
+        <Link
+          className="dropdown-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#faqs_toefl23"
+        >
+          Q: Can I take the TOEFL exam more than once?
+        </Link>
+        <div className="collapse" id="faqs_toefl23">
+          <p>
+            <span>Answer:</span>
+            <p>
+              Yes, you can take the TOEFL exam as many times as you want, but
+              there must be at least 12 days between tests.
+            </p>
+            <p>
+              <span> In conclusion </span>, the TOEFL exam can be challenging, but with the
+              right preparation and mindset, you can achieve success. By
+              focusing on each section of the exam individually, setting goals,
+              and staying organized, you can improve your English language
+              skills and achieve your desired TOEFL score. Let’s start your
+              journey to TOEFL and Study Abroad success story today!
+            </p>
+          </p>
         </div>
       </div>
 
