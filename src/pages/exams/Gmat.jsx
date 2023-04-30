9410222aa6c2cf12cc7ff3da4b899599d812457f
Victor Yohanna <victoryohanna@gmail.com>
2023-05-01 00:44:00 +0100

    add gmat page content

--- src/pages/exams/Gmat.jsx
+++ src/pages/exams/Gmat.jsx
@@ -1,10 +1,378 @@
+import "../../sass/gre.scss";
 
-const GMAT = ()=>{
-    return(
-        <div>
-            <h1>GMAT</h1>
+const GMAT = () => {
+  return (
+    <div className="gre-container">
+      <div className="gre-header">
+        <h2>GMAT Exam 2023 </h2>
+        <h5>
+          [Dates, Registration, Fees, Eligibility, Syllabus, Pattern,
+          Preparation, Result & Score]
+        </h5>
+      </div>
+      <div className="gre-details ">
+        <p>
+          Welcome to our GMAT exam preparation and registration page, your
+          ultimate guide for all things related to the GMAT exam. Whether you
+          are a prospective student or a professional looking to further your
+          career, you have come to the right place. In this comprehensive guide,
+          we will cover everything you need to know about the GMAT exam,
+          including its format, content, scoring, and more.
+        </p>
+        <p>
+          The Graduate Management Admission Test (GMAT) is a crucial exam for
+          individuals seeking to pursue a graduate degree in business
+          administration or management. The GMAT exam is recognized and accepted
+          by more than 7,000 programs at approximately 2,300 graduate business
+          schools around the world. It measures your abilities in Analytical
+          Writing Assessment (one essay task), Integrated Reasoning (12
+          multiple-choice questions), Quantitative Reasoning (31 multiple-choice
+          questions), and Verbal Reasoning (36 multiple-choice questions), all
+          of which are essential skills for success in the business world.
+        </p>
+        <p>
+          The exam is computer-based and takes about 3.5 hours to complete,
+          including breaks. During the exam, you will be presented with a series
+          of questions that will assess your abilities in the above areas. The
+          exam is designed to test your critical thinking skills and your
+          ability to make sound judgments in real-world business scenarios.
+        </p>
+        <p>
+          Preparing for the GMAT exam can be challenging, but with the right
+          resources and guidance, you can improve your chances of success. Apart
+          from allowing you to register for the exam and providing you with
+          adequate information, this platform also offers a variety of GMAT exam
+          preparation materials, including study guides, practice exams, and
+          coaching services. We are committed to helping you achieve your goals
+          and reach your full potential.
+        </p>
+        <div className="collapse-content">
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#greFormat"
+          >
+            <h6>GMAT EXAM FORMAT 2023</h6>
+          </button>
+          <div className="collapse" id="greFormat">
+            <p>
+              The GMAT exam is a computer-based test that consists of four
+              sections: Analytical Writing Assessment, Integrated Reasoning,
+              Quantitative Reasoning, and Verbal Reasoning. The exam is designed
+              to test your critical thinking skills, reasoning abilities, and
+              communication skills, all of which are essential for success in
+              the business world.
+            </p>
+            <p>
+              The GMAT exam takes about 3.5 hours to complete, including two
+              optional breaks. You will have 30 minutes for the Analytical
+              Writing Assessment section, 30 minutes for the Integrated
+              Reasoning section, and 62 minutes each for the Quantitative
+              Reasoning and Verbal Reasoning sections. The exam is designed to
+              be challenging, so it is important to pace yourself and manage
+              your time wisely.
+            </p>
+            <p>
+              The GMAT exam is scored on a scale of 200-800, with 10-point
+              increments. The Analytical Writing Assessment section is scored on
+              a scale of 0-6, in half-point increments, and the Integrated
+              Reasoning section is scored on a scale of 1-8, in one-point
+              increments. The Quantitative Reasoning and Verbal Reasoning
+              sections are each scored on a scale of 0-60, in one-point
+              increments.
+            </p>
+            <p>
+              Your GMAT score is an important factor in the business school
+              admissions process. It is used by graduate business schools to
+              assess your readiness for the academic rigours of an MBA or other
+              business degree program. A higher GMAT score can also increase
+              your chances of receiving scholarships or other financial aid.
+            </p>
+          </div>
         </div>
-    )
-}
+        <div className="collapse-content">
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#whyGRE"
+          >
+            <h6>Why Should You Take the GMAT Exam in 2023</h6>
+          </button>
+          <div className="collapse" id="whyGRE">
+            <ul>
+              <li>
+                <span>Essential for Business School Admission: </span>
+                One of the primary reasons for taking the GMAT is that it is a
+                requirement for admission to most graduate business programs,
+                including MBA programs. Your GMAT score is one of the key
+                factors that admissions committees consider when evaluating your
+                application. A strong GMAT score can improve your chances of
+                getting into the business school of your choice.
+              </li>
+              <li>
+                <span>Demonstrates Business School Readiness: </span>The GMAT
+                exam is designed to test the skills that are essential for
+                success in business school, including analytical thinking,
+                problem-solving, and communication skills. By taking the GMAT
+                and performing well, you demonstrate to admissions committees
+                that you have the skills and knowledge necessary to excel in a
+                graduate business program.
+              </li>
+              <li>
+                <span>Provides Career Advancement Opportunities: </span>A
+                graduate business degree can open up a world of career
+                advancement opportunities, from higher-paying jobs to
+                executive-level positions. By taking the GMAT and earning a high
+                score, you can increase your chances of being accepted into a
+                top-ranked business program and ultimately achieve your career
+                goals.
+              </li>
+              <li>
+                <span>
+                  Demonstrates Commitment to Professional Development:{" "}
+                </span>
+                Taking the GMAT and pursuing a graduate business degree also
+                shows a commitment to professional development and a desire to
+                improve your skills and knowledge in the business world. This
+                commitment can be attractive to potential employers and can help
+                you stand out in a crowded job market.
+              </li>
+              <li>
+                <span>Opens Doors to Networking Opportunities: </span>Graduate
+                business programs often provide students with access to
+                networking opportunities with other business professionals,
+                alumni, and industry experts. By taking the GMAT and pursuing a
+                graduate business degree, you can expand your professional
+                network and gain valuable connections that can help you advance
+                your career.
+              </li>
+            </ul>
+            <p>
+              Taking the GMAT is important for anyone who wants to pursue a
+              graduate business degree and advance their career in the business
+              world. By demonstrating your skills, knowledge, and commitment to
+              professional development, you can improve your chances of success
+              in the highly competitive business environment.
+            </p>
+          </div>
+        </div>
+        <div className="collapse-content">
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#scoreFormat"
+          >
+            <h6>Composition and Scoring of the GMAT Exam in 2023</h6>
+          </button>
+          <div className="collapse" id="scoreFormat">
+            <p>
+              The GMAT exam is scored on a scale of 200-800, with each section
+              scored separately on different scales. Although derived from your
+              total performance on the entire GMAT test, the total overall
+              scaled scores (because it is scaled differently) cannot be
+              obtained by adding the scores from individual sections. The scores
+              are valid for only 5 years from the test date.
+            </p>
+
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">GMAT Section</th>
+                  <th scope="col">Task</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Scaled Score Range</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Analytical Writing Assessment (AWA)</td>
+                  <td>
+                    Argument analysis and <br /> communication in the form of a
+                    critique
+                  </td>
+                  <td>30 mins</td>
+                  <td>0 - 6</td>
+                </tr>
+                <tr>
+                  <td>Integrated Reasoning (IR) </td>
+                  <td>
+                    12 Multiple Choice Questions <br />
+                    Two-Part Analysis, Multi-Source Reasoning, <br /> Graphic
+                    Interpretation, Table analysis
+                  </td>
+                  <td>30 mins</td>
+                  <td>1 – 8</td>
+                </tr>
+                <tr>
+                  <td>Quantitative</td>
+                  <td>
+                    31 Multiple Choice Questions <br />
+                    Data Sufficiency (DS), Problem Solving (PS)
+                  </td>
+                  <td>62 mins</td>
+                  <td>6 – 51</td>
+                </tr>
+                <tr>
+                  <td>Verbal </td>
+                  <td>
+                    36 Multiple Choice Questions: <br />
+                    Reading Comprehension (RC), <br /> Critical Reasoning (CR),{" "}
+                    <br />
+                    Sentence Correction (SC)
+                  </td>
+                  <td>65 mins </td>
+                  <td>6 - 51</td>
+                </tr>
+                <tr>
+                  <td colSpan="2">Total Scaled Score</td>
+                  <td>3hrs and 7mins</td>
+                  <td>200 – 800</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="collapse-content">
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#myth-burster"
+          >
+            <h6>
+              GMAT Exam in 2023; Myth-Bursters [Myths Vs Realities of GMAT]
+            </h6>
+          </button>
+          <div className="collapse" id="myth-burster">
+            <ul>
+              <li>
+                GMAT scores are the only factor that business schools consider
+                for admission.The GRE is all about memorization.
+                <p>
+                  Reality: While a strong GMAT score is important, business
+                  schools consider a range of factors when evaluating
+                  candidates, including work experience, academic record, and
+                  leadership potential.
+                </p>
+              </li>
+              <li>
+                Only people with a background in business should take the GMAT.
+                <p>
+                  Reality: The GMAT is designed to test skills that are
+                  important for success in business school, but it is not
+                  limited to people with a background in business. Students from
+                  a variety of academic backgrounds can take the GMAT and
+                  succeed.
+                </p>
+              </li>
+              <li>
+                You need to have a high level of math skills to do well on the
+                GMAT.
+                <p>
+                  Reality: While the GMAT does test math skills, it is designed
+                  to assess your ability to reason logically and solve problems.
+                  You do not need to be a math expert to do well on the GMAT.
+                </p>
+              </li>
+              <li>
+                The GMAT is all about memorization.
+                <p>
+                  Reality: The GMAT is designed to test your critical thinking
+                  and analytical skills, not your ability to memorize
+                  information. The exam focuses on your ability to analyze
+                  information, draw conclusions, and make decisions.
+                </p>
+              </li>
+              <li>
+                You only need to take the GMAT once.
+                <p>
+                  Reality: While you can take the GMAT multiple times, it is
+                  important to prepare thoroughly for each attempt. Your scores
+                  from multiple attempts will be visible to business schools, so
+                  it is important to show improvement over time.
+                </p>
+              </li>
+              <li>
+                You can't prepare for the GMAT.
+                <p>
+                  Reality: The GMAT is a standardized test, which means that it
+                  is possible to prepare for it. There are a variety of
+                  resources available, including prep courses, study materials,
+                  and practice tests. With sufficient preparation, you can
+                  improve your chances of success on the GMAT.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="collapse-content">
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#myth-burster2"
+          >
+            <h6>
+              Study Tips and Strategies (Preparing for the GMAT Exam in 2023)
+            </h6>
+          </button>
+          <div className="collapse" id="myth-burster2">
+            <p>
+              Preparing for the GMAT exam can be a daunting task, but with the
+              right tips and strategies, you can increase your chances of
+              success. Here are some tips and strategies to help you prepare for
+              the GMAT exam:
+            </p>
+            <ul>
+              <li>
+                <span>Understand the format and content of the exam: </span> The
+                GMAT exam consists of four sections - Analytical Writing
+                Assessment, Integrated Reasoning, Quantitative, and Verbal.
+                Familiarize yourself with the format and content of each section
+                so that you can develop an effective study plan.
+              </li>
+              <li>
+                <span>Create a study plan: </span>Set aside dedicated time each
+                week to study for the GMAT exam. Create a study plan that works
+                for you and stick to it.
+              </li>
+              <li>
+                <span>Use official GMAT materials: </span>The GMAT exam is
+                created by the Graduate Management Admission Council (GMAC), so
+                it's important to use official GMAT materials when preparing for
+                the exam. These materials are designed to closely mimic the
+                actual exam, giving you a better idea of what to expect.
+              </li>
+              <li>
+                <span>Practice, practice, practice: </span>Practice is essential
+                when preparing for the GMAT exam. Take as many practice tests as
+                possible to get a sense of the timing and types of questions
+                you'll encounter on the exam. Analyze your results to identify
+                areas where you need to improve.
+              </li>
+              <li>
+                <span>Focus on your weaknesses: </span>Once you've identified
+                your weaknesses, focus on improving them. For example, if you
+                struggle with math, focus on studying math concepts and
+                practicing math problems.
+              </li>
+              <li>
+                <span>Take breaks and stay motivated: </span>Studying for the
+                GMAT exam can be mentally exhausting, so it's important to take
+                breaks and stay motivated. Take breaks to rest your mind and
+                recharge, and find ways to stay motivated, such as setting goals
+                or studying with a friend.
+              </li>
+              <li>
+                <span>Get help if needed: </span>If you're struggling with a
+                particular aspect of the exam, don't be afraid to seek help.
+                Consider taking a prep course, hiring a tutor, or working with a
+                study group to get the support you need.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default GMAT;
