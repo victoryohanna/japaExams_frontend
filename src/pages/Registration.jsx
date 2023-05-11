eec5232cbf6094da9e991b3a750b877ab9f62a14
Victor Yohanna <victoryohanna@gmail.com>
2023-05-11 05:58:44 +0100

    modified registration and add styles

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,10 +1,75 @@
+import { useState } from "react";
+
+import "../sass/register.scss";
+import { data } from "../components/elements";
 
 const Register = () => {
-    return (
-        <div className="container">
-            <h1>Registration</h1>
+  const [examType, setExamType] = useState();
+
+  return (
+    <div className="container">
+      <div className="form_section">
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  What type of exams do you want to write?
+                </label>
+                <select className="form-select" aria-label="">
+                  <option> Select</option>
+                  {data.map((item, i) => {
+                    return (
+                      <option
+                        onChange={(e) => setExamType(e.target.value)}
+                        key={i}
+                      >
+                        {item.title}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  Where do you want to write your exams?
+                </label>
+                <select className="form-select" aria-label="">
+                  <option>{examType}</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  Select Date of Examination
+                </label>
+                <select className="form-select" aria-label="">
+                  <option></option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div className="button_next">
+                <button>Next</button>
+              </div>
+            </form>
+          </div>
         </div>
-      );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Register;
